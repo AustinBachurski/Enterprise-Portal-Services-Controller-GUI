b620e69bc6232c1dc42575fdaba2047446f122a9
Austin Bachurski <58963315+AustinBachurski@users.noreply.github.com>
2023-06-06 08:21:02 -0600

    Refactor and add additional functionality.

--- src/Application.hpp
+++ src/Application.hpp
@@ -3,10 +3,12 @@
 
 #include "Constants.hpp"
 #include "CustomRichTextCtrl.hpp"
-#include "Credentials.hpp"
+#include "Configuration.hpp"
 #include "EnterCredentials.hpp"
-#include "Services.hpp"
+#include "PortalServerControls.hpp"
 #include <chrono>
+#include <future>
+#include <memory>
 #include <string>
 #include <string_view>
 #include <thread>
@@ -17,68 +19,43 @@
 #include <wx/wx.h>
 #include <wx/progdlg.h>
 
-// Launch Application.
 class Application : public wxApp
 {
 public:
-	virtual bool OnInit();
+	bool OnInit() override;
 };
 
-// Main Window Frame.
 class Frame : public wxFrame
 {
 public:
     Frame(const std::string&& title);
+	~Frame();
 
 private:
-	int m_caretPosition;
-	Credentials m_credentials;
-	wxFindReplaceData m_findSettings;
-	std::string m_findString;
-	Services m_mapServices;
+	Configuration m_configuration;
+	// Find text from top to bottom by default.
+	wxFindReplaceData m_findOptions{ wxFR_DOWN };
+	std::unique_ptr<PortalServerControls> m_portalServerControl;
+	int m_serverCommandMethod;
+	CustomRichTextCtrl* m_statusText{ nullptr };
+	wxMenuItem* m_menuModeBatch{ nullptr };
+	wxMenuItem* m_menuModeSequential{ nullptr };
 
-	// Text box that displays the service state information.
-	CustomRichTextCtrl* m_statusText;
-
-	// Display confirmation dialog.
 	void areYouSure(const std::string_view command);
-
-	// Loads displayed server status into the clipboard.
 	void copyServerStatus();
-
-	// Updates m_statusText with all map services.
 	void displayStatusAll();
-
-	// Updates m_statusText with "STARTED" map services..
 	void displayStatusStarted();
-
-	// Updates m_statusText with "STOPPED" map services.
 	void displayStatusStopped();
-
-	// Displays a window to allow user to enter Enterprise Portal credentials.
+	void displayWelcomeMessage();
 	void enterCredentials();
-
-	/*
-	Triggers the desired action based on event.GetId()
-	@param wxEvent from menu selection.
-	*/
+	void exportJson(MenuID mode);
+	void keyboardShortcuts(wxKeyEvent& event);
 	void menuAction(wxCommandEvent& event);
-
-	/* 
-	Displays a progress dialog, updates the "status" of
-	services in m_serviceInformation, and updates m_statusText.
-	 */
 	void refreshStatus();
-
-	/*
-	Displays a progress dialog and calls mapServices.sendServerCommand
-	with the desired command.
-	@param Constants::Commands::START or Constants::Commands::STOP
-	*/
-	void sendCommand(const std::string_view command);
-
-	// Displays a wxFindReplaceDialog - only the "Find" portion.
+	void sendBatchCommand(const std::string_view command);
+	void sendSequentialCommand(const std::string_view command);
 	void showFindTextPrompt();
+	void updateCredentials();
 };
 
 #endif
