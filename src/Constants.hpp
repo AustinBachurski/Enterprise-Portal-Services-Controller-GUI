46e5f960ff3cf264f2515f481db2d392b39cb84e
Austin Bachurski <58963315+AustinBachurski@users.noreply.github.com>
2023-06-06 08:21:26 -0600

    Refactor and add additional functionality.

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -6,177 +6,122 @@
 #include <string>
 #include <string_view>
 
-// Constant values.
+enum MenuID
+{
+	quit,
+	changeCredentials,
+	setMethodSequential,
+	setMethodBatch,
+	find,
+	copySelection,
+	copyAll,
+	refresh,
+	showAll,
+	showStarted,
+	showStopped,
+	exportStart,
+	exportStop,
+	start,
+	stop,
+};
+
+struct threadState
+{
+	int progressValue{};
+	int progressMax{};
+	wxString message{};
+	bool working{};
+};
+
 namespace Constants
 {
-	// Server commands.
 	namespace Commands
 	{
-		// STARTED
 		inline constexpr std::string_view START{ "STARTED" };
-		
-		// STOPPED
 		inline constexpr std::string_view STOP{ "STOPPED" };
+		inline constexpr int batchMode{ 0 };
+		inline constexpr int sequentialMode{ 1 };
 	}
 
 	namespace JSON
 	{
-		// "},
 		inline const std::string closeBrace{ "\"}," };
-
-		// "foldername":"
 		inline const std::string folderName{ "\"folderName\":\""  };
-
-		// {
 		inline const std::string openBrace{ "{" };
-
-		// ","serviceName":"
 		inline const std::string serviceName{ "\",\"serviceName\":\"" };
-
-		// ","type":"
 		inline const std::string type{ "\",\"type\":\"" };
 	}
 
-	// wxWidgets MenuBar ids.
-	namespace MenuID
-	{
-		inline constexpr int changeCredentials = 1;
-		inline constexpr int quit = 2;
-		inline constexpr int find = 3;
-		inline constexpr int copyAll = 4;
-		inline constexpr int copySelection = 5;
-		inline constexpr int refresh = 6;
-		inline constexpr int showAll = 7;
-		inline constexpr int showStarted = 8;
-		inline constexpr int showStopped = 9;
-		inline constexpr int start = 10;
-		inline constexpr int stop = 11;
-	}
-
-	// Modal window text. 
 	namespace Messages
 	{
-		/*
-		Credentials were not found, please restart the tool and
-		 enter your Enterprise Portal credentials whe prompted.
-		 Tool functionality has been disabled for this session.
-		*/
-		inline const std::string_view credentialsNotFound{
-			"Credentials were not found, please restart the tool\nand "
-			"enter your Enterprise Portal credentials when prompted.\n"
-			"Tool functionality has been disabled for this session." };
+		inline const wxString credentialsRequired{
+			"Credentials must be valid for the tool to function correctly." };
 
-		// START all services on the server? 
-		inline const wxString confirmStart{
-			"START all services on the server?" };
-
-		// STOP all services on the server? 
-		inline const wxString confirmStop{
-			"STOP all services on the server?" };
-
-		/*
-		'Refresh Server Status' in the
-		context menu to update the status display.
-		*/
 		inline constexpr std::string_view initialStatus{
 			"Use 'Refresh Server Status' in the\n"
 			"context menu to update the status display." };
 		
-		// Warning: This will take a few minutes. 
 		inline const wxString durationWarning{
 			"\n\nWarning: This will take a few minutes." };
 		
-		/*
-		The tool failed to create the .\config directory.
-		Please try creating the directory manually in the same
-		directory as the tool.
-		*/
 		inline const wxString directoryError{
 			"The tool failed to create the \".\\config\" directory.\n"
 			"Please try creating the directory manually"
-			"in the same directory as the tool.",
-		};
+			"in the same directory as the tool." };
 
-		/*
-		Unable to generate config file, check your file/filder permissions.
-		Tool requires ".\config\config.ini" to function properly.
-		*/
 		inline const wxString fileError{
 			"Unable to generate config file, check your file/folder permissions.\n"
-			"Tool requires \".\\config\\config.ini\" to function properly."
-		};
+			"Tool requires \".\\config\\config.ini\" to function properly." };
 
-		// {"status": "Services are already in requested state."} 
+		inline const wxString gathering{ "Gathering current server status." };
+
 		inline const nlohmann::json jsonError = nlohmann::json::parse(
 			R"({"status": "Error"})");
-
-		/*
-		No response was received from the server, is your url correct?\n
-		*/
-		inline const wxString noResponse{
-			"No response was received from the server, is your url correct?\n"
-		};
-
-		// {"status": "Services are already in requested state."} 
-		inline const nlohmann::json nothingToDo = nlohmann::json::parse(
+		
+		inline const nlohmann::json jsonNothingToDo = nlohmann::json::parse(
 			R"({"status": "Services are already in requested state."})");
 
-		// Refreshing server status. 
-		inline const wxString refreshing{ "Refreshing server status." };
+		inline const wxString noResponse{
+			"No response was received from the server, is your url correct?\n" };
 
-		/*
-		No response received from server.
-		Please verisy your Enterprise Portal Credentials and restart the tool.
-		Tool functionality has been disabled.
-		*/
 		inline const std::string_view serverError{
-			"No response recieved from server.\nPlease "
-			"verify your Enterprise Portal Credentials and restart the tool.\n"
-			"Tool functionality has been disabled for this session."
-		};
+			"Error communicating with the server.\n"
+			"You can change your credentials by going to\n"
+			"File->Change Enterprise Portal Credentials." };
 
-		// An error occurred while attempting to aquire a token: 
+		inline const nlohmann::json success = nlohmann::json::parse(
+			R"({"status": "success"})");
+
 		inline const wxString tokenError{
-			"An error occurred while attempting ao aquire a token:\n" };
-		
-		// Sending 'Start All' command to server. 
-		inline const wxString starting{
-			"Sending 'Start All' command to server." };
-		
-		// Sending 'Stop All' command to server. 
-		inline const wxString stopping{
-			"Sending 'Stop All' command to server." };
-		
-		// Assembling commands and sending to server, please wait... 
+			"An error occurred while attempting to aquire a token.\n\n" };
+
+		inline const wxString waiting{
+			"Commands sent, awaiting server responses."
+			"\nThis can take a couple minutes..." };
+
 		inline const wxString waitCommand{
-			"Assembling commands and sending to server, please wait..." };
+			"Assembling command and sending to server, please wait..." };
 		
-		// Retrieving status from server, please wait... 
 		inline const wxString waitRefresh{
 			"Retrieving status from server, please wait..." };
 	}
 
-	// Option flags for "Find" dialog. 
+	// Option flags for "Find" dialog from wxWidgets. 
 	namespace SearchMethod
 	{
 		inline constexpr int topToBottom{ 1 };
-		inline constexpr int matchWord_topToBottom{ 3 };
-		inline constexpr int matchCase_topToBottom{ 5 };
-		inline constexpr int matchBoth_topToBottom{ 7 };
-
+		inline constexpr int topToBottomMatchWord{ 3 };
+		inline constexpr int topToBottomMatchCase{ 5 };
+		inline constexpr int topToBottom_matchBoth{ 7 };
 		inline constexpr int bottomToTop{ 0 };
-		inline constexpr int matchWord_bottomToTop{ 2 };
-		inline constexpr int matchCase_bottomToTop{ 4 };
-		inline constexpr int matchBoth_bottomToTop{ 6 };
+		inline constexpr int bottomToTopMatchWord{ 2 };
+		inline constexpr int bottomToTopMatchCase{ 4 };
+		inline constexpr int bottomToTopMatchBoth{ 6 };
 	}
 
-	// Words, yep yep yep.
 	namespace Words
 	{
-		// folders
 		inline std::string_view folders{ "folders" };
-
-		// services
 		inline std::string_view services{ "services" };
 	}
 }
