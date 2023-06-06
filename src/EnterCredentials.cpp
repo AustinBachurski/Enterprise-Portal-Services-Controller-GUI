69140bf4b2c868e4f4bdb7b52ff1c80a7db9e96e
Austin Bachurski <58963315+AustinBachurski@users.noreply.github.com>
2023-06-06 08:24:14 -0600

    Refactor and add additional functionality.

--- src/EnterCredentials.cpp
+++ src/EnterCredentials.cpp
@@ -1,43 +1,38 @@
 #include "EnterCredentials.hpp"
 
-EnterCredentials::EnterCredentials(wxWindow* parent, Credentials& credentials) :
-    wxDialog(parent, wxID_ANY, "Enter Enterprise Portal Information"),
-    m_okPressed{ false }
+EnterCredentials::EnterCredentials(
+    wxWindow* parent,
+    Configuration& configuration)
+    : wxDialog(parent, wxID_ANY, "Enter Enterprise Portal Information") 
 {
     SetClientSize(350, 135);
-    //CenterOnScreen();
     
-    // URL Input Box.
     wxStaticText* urlPrompt = new wxStaticText(this, wxID_ANY,
         "Enterprise Portal URL -", wxPoint(10, 14), wxSize(-1, 25));
     m_urlEntryBox = new wxTextCtrl(this, wxID_ANY,
         "i.e. https://maps.city.name.state.us/",
         wxPoint(131, 10), wxSize(209, 25));
 
-    // Username Input Box.
     wxStaticText* usernamePrompt = new wxStaticText(this, wxID_ANY,
         "Enterprise Portal Username -", wxPoint(10, 44), wxSize(-1, 25));
     m_usernameEntryBox = new wxTextCtrl(this, wxID_ANY,
         wxEmptyString, wxPoint(163, 40), wxSize(177, 25));
 
-    // Password Input Box.
     wxStaticText* passwordPrompt = new wxStaticText(this, wxID_ANY,
         "Enterprise Portal Password -", wxPoint(10, 74), wxSize(-1, 25));
     m_passwordEntryBox = new wxTextCtrl(this,wxID_ANY,
         wxEmptyString, wxPoint(160, 70), wxSize(180, 25),
         wxTE_PASSWORD | wxTE_PROCESS_ENTER);
     m_passwordEntryBox->Bind(wxEVT_TEXT_ENTER,
-        [this, &credentials](wxCommandEvent& event)
-        { captureCredentials(event, credentials); });
+        [this, &configuration](wxCommandEvent& event)
+        { captureCredentials(event, configuration); });
         
-    // OK Button.
     wxButton* OkButton = new wxButton(this, wxID_OK,
         "OK", wxPoint(173, 105), wxSize(85, -1));
     OkButton->Bind(wxEVT_BUTTON, 
-        [this, &credentials](wxCommandEvent& event)
-        { captureCredentials(event, credentials); });
+        [this, &configuration](wxCommandEvent& event)
+        { captureCredentials(event, configuration); });
 
-    // Cancel Button.
     wxButton* cancelButton = new wxButton(this, wxID_CANCEL,
         "Cancel", wxPoint(87, 105), wxSize(85, -1));
     cancelButton->Bind(wxEVT_BUTTON,
@@ -46,8 +41,8 @@ EnterCredentials::EnterCredentials(wxWindow* parent, Credentials& credentials) :
             EndModal(wxCANCEL);
             Destroy();
 
-            // Even when pressing "OK" the close command for the window would
-            // trigger the 'required' popup window, m_okPressed prevents this.
+            // Window closing would trigger the 'required' popup window
+            // regardless of why it closed, m_okPressed prevents this.
             if (!m_okPressed)
             {
                 wxMessageDialog* required = new wxMessageDialog(this,
@@ -58,18 +53,15 @@ EnterCredentials::EnterCredentials(wxWindow* parent, Credentials& credentials) :
         });
 }
 
-/*
-Captures text from the text entry boxes and sends to Credentials::setConfig().
-*/ 
 void EnterCredentials::captureCredentials(
-    wxCommandEvent& event,Credentials& credentials)
+    wxCommandEvent& event, Configuration& configuration)
 {
     m_okPressed = true;
 
-    credentials.setConfig(
-        std::move(m_urlEntryBox->GetValue().ToStdString()),
-        std::move(m_usernameEntryBox->GetValue().ToStdString()),
-        std::move(m_passwordEntryBox->GetValue().ToStdString()));
+    configuration.writeConfig(
+        m_urlEntryBox->GetValue().ToStdString(),
+        m_usernameEntryBox->GetValue().ToStdString(),
+        m_passwordEntryBox->GetValue().ToStdString());
 
     Close();
 }
