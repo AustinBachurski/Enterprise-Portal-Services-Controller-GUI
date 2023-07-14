8d269f819c3ff123bde63259cf3576052f2b0057
Austin Bachurski <58963315+AustinBachurski@users.noreply.github.com>
2023-07-14 09:22:57 -0600

    Add sizers to credentials window.

--- src/EnterCredentials.cpp
+++ src/EnterCredentials.cpp
@@ -5,23 +5,35 @@ EnterCredentials::EnterCredentials(
     Configuration& configuration)
     : wxDialog(parent, wxID_ANY, "Enter Enterprise Portal Information") 
 {
-    SetClientSize(350, 135);
-    
-    wxStaticText* urlPrompt{ new wxStaticText(this, wxID_ANY,
-        "Enterprise Portal URL -", wxPoint(10, 14), wxSize(-1, 25)) };
-    m_urlEntryBox = new wxTextCtrl(this, wxID_ANY,
+    wxPanel* urlPanel{ new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize) };
+    wxPanel* usernamePanel{ new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize) };
+    wxPanel* passwordPanel{ new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize) };
+    wxPanel* buttonPanel{ new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize) };
+
+    wxBoxSizer* urlSizer{ new wxBoxSizer(wxHORIZONTAL) };
+    wxBoxSizer* usernameSizer{ new wxBoxSizer(wxHORIZONTAL) };
+    wxBoxSizer* passwordSizer{ new wxBoxSizer(wxHORIZONTAL) };
+    wxBoxSizer* buttonSizer{ new wxBoxSizer(wxHORIZONTAL) };
+    wxBoxSizer* stacker{ new wxBoxSizer(wxVERTICAL) };
+
+    wxStaticText* urlPrompt{ new wxStaticText(urlPanel, wxID_ANY,
+        "Enterprise Portal URL", wxDefaultPosition, wxDefaultSize) };
+
+    m_urlEntryBox = new wxTextCtrl(urlPanel, wxID_ANY,
         "i.e. https://maps.city.name.state.us/",
-        wxPoint(131, 10), wxSize(209, 25));
+        wxDefaultPosition, wxSize(250, -1));
 
-    wxStaticText* usernamePrompt{ new wxStaticText(this, wxID_ANY,
-        "Enterprise Portal Username -", wxPoint(10, 44), wxSize(-1, 25)) };
-    m_usernameEntryBox = new wxTextCtrl(this, wxID_ANY,
-        wxEmptyString, wxPoint(163, 40), wxSize(177, 25));
+    wxStaticText* usernamePrompt{ new wxStaticText(usernamePanel, wxID_ANY,
+        "Enterprise Portal Username", wxDefaultPosition, wxDefaultSize) };
 
-    wxStaticText* passwordPrompt{ new wxStaticText(this, wxID_ANY,
-        "Enterprise Portal Password -", wxPoint(10, 74), wxSize(-1, 25)) };
-    m_passwordEntryBox = new wxTextCtrl(this,wxID_ANY,
-        wxEmptyString, wxPoint(160, 70), wxSize(180, 25),
+    m_usernameEntryBox = new wxTextCtrl(usernamePanel, wxID_ANY,
+        wxEmptyString, wxDefaultPosition, wxSize(250, -1));
+
+    wxStaticText* passwordPrompt{ new wxStaticText(passwordPanel, wxID_ANY,
+        "Enterprise Portal Password", wxDefaultPosition, wxDefaultSize) };
+
+    m_passwordEntryBox = new wxTextCtrl(passwordPanel,wxID_ANY,
+        wxEmptyString, wxDefaultPosition, wxSize(250, -1),
         wxTE_PASSWORD | wxTE_PROCESS_ENTER);
     m_passwordEntryBox->Bind(wxEVT_TEXT_ENTER,
         [this, &configuration](wxCommandEvent& event)
@@ -30,8 +42,8 @@ EnterCredentials::EnterCredentials(
             captureCredentials(event, configuration);
         });
         
-    wxButton* OkButton{ new wxButton(this, wxID_OK,
-        "OK", wxPoint(173, 105), wxSize(85, -1)) };
+    wxButton* OkButton{ new wxButton(buttonPanel, wxID_OK,
+        "OK", wxDefaultPosition, wxDefaultSize) };
     OkButton->Bind(wxEVT_BUTTON, 
         [this, &configuration](wxCommandEvent& event)
         { 
@@ -39,8 +51,8 @@ EnterCredentials::EnterCredentials(
             captureCredentials(event, configuration);
         });
 
-    wxButton* cancelButton{ new wxButton(this, wxID_CANCEL,
-        "Cancel", wxPoint(87, 105), wxSize(85, -1)) };
+    wxButton* cancelButton{ new wxButton(buttonPanel, wxID_CANCEL,
+        "Cancel", wxDefaultPosition, wxDefaultSize) };
     cancelButton->Bind(wxEVT_BUTTON,
         [this, &configuration](wxCommandEvent& event)
         {
@@ -57,6 +69,33 @@ EnterCredentials::EnterCredentials(
                 required->ShowModal();
             }
         });
+
+    urlSizer->Add(0, 0, wxEXPAND, 0);
+    urlSizer->Add(urlPrompt, 0, wxALIGN_CENTER_VERTICAL, 0);
+    urlSizer->Add(m_urlEntryBox, 0, wxALIGN_CENTER_VERTICAL | wxLEFT, 5);
+    urlPanel->SetSizerAndFit(urlSizer);
+
+    usernameSizer->Add(usernamePrompt, 0, wxALIGN_CENTER_VERTICAL, 0);
+    usernameSizer->Add(m_usernameEntryBox, 0, wxALIGN_CENTER_VERTICAL | wxLEFT, 5);
+    usernamePanel->SetSizerAndFit(usernameSizer);
+
+    passwordSizer->Add(0, 0, wxEXPAND, 0);
+    passwordSizer->Add(passwordPrompt, 0, wxALIGN_CENTER_VERTICAL, 0);
+    passwordSizer->Add(m_passwordEntryBox, 0, wxALIGN_CENTER_VERTICAL | wxLEFT, 5);
+    passwordPanel->SetSizerAndFit(passwordSizer);
+    
+    buttonSizer->Add(0, 0, wxEXPAND);
+    buttonSizer->Add(OkButton, 0, wxALIGN_CENTER | wxRIGHT, 5);
+    buttonSizer->Add(cancelButton, 0, wxALIGN_CENTER);
+    buttonPanel->SetSizerAndFit(buttonSizer);
+
+    stacker->Add(urlPanel, 1, wxEXPAND | wxTOP | wxLEFT | wxRIGHT, 5);
+    stacker->Add(usernamePanel, 1, wxEXPAND | wxTOP | wxLEFT | wxRIGHT, 5);
+    stacker->Add(passwordPanel, 1, wxEXPAND | wxTOP | wxLEFT | wxRIGHT, 5);
+    stacker->Add(buttonPanel, 1, wxEXPAND | wxTOP | wxLEFT | wxRIGHT, 5);
+    stacker->Add(0, 5);
+
+    SetSizerAndFit(stacker);
 }
 
 void EnterCredentials::captureCredentials(
